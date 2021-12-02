ef32d1cfa31bdb83a6d445075e78c0dc1a54821d
adriennefranke <18449966+adriennefranke@users.noreply.github.com>
2021-12-02 12:07:23 -0600

    Day 1 Part 2 progress

--- day_1.rb
+++ day_1.rb
@@ -1,5 +1,9 @@
 # Read in the data
 input = File.readlines('day_1_input').map(&:to_i)
 
-result = input.each_cons(2).count { |pair| pair.first < pair.last }
-puts result
+# Part 1
+# result = input.each_cons(2).count { |pair| pair.first < pair.last }
+# puts result
+
+# Part 2
+puts input.each_cons(3) { |a| p a.sum}.class
